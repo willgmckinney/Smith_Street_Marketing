da62d5fcc91f8e83d644a871805830fdfe12c60d
William McKinney <wmckinney@under.io>
2024-06-23 22:54:44 -0500

    modifying the site a bit with beiefs section and wording changes

--- src/pages/HomePage/Components/HeroBanner/index.tsx
+++ src/pages/HomePage/Components/HeroBanner/index.tsx
@@ -3,7 +3,7 @@ import { Right } from "./Right";
 
 export const HeroBanner = () => {
   return (
-    <div className="flex flex-row items-center justify-between h-screen bg-[#00262D] text-white">
+    <div className="flex flex-row items-center justify-between bg-[#00262D] text-white h-[90%]">
       <Left />
       <Right />
     </div>
